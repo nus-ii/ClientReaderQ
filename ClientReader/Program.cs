a2c6df2dee3a2a65b3fd2d4a43b788861dcd3118
Илья Жаров <i.zharov@virtusystems.ru>
2016-06-08 00:00:22 +0300

    1

--- ClientReader/Program.cs
+++ ClientReader/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,22 @@ namespace ClientReader
 	{
 		static void Main(string[] args)
 		{
+			string filePath = @"target.csv";
+			var result = StreamFile(filePath);
+
+			Console.ReadLine();
+		}
+
+		private static StreamReader StreamFile(string filePath)
+		{
+			var text = File.ReadAllText(filePath);
+			MemoryStream stream = new MemoryStream();
+			StreamWriter writer = new StreamWriter(stream);
+			writer.Write(text);
+			writer.Flush();
+			stream.Position = 0;
+			var result = new StreamReader(stream);
+			return result;
 		}
 	}
 }
